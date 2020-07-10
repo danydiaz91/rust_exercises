70478f000d61e445e666059aea528726e1562575
Dany Diaz <dany.diaz91@hotmail.com>
2020-07-10 13:42:59 -0500

    feat: validate if belongs to the same institution.

--- exercise_3/src/lib.rs
+++ exercise_3/src/lib.rs
@@ -10,6 +10,27 @@ pub enum EducationalStage {
     College {program: String}
 }
 
+impl EducationalStage {
+    pub fn same_variant(&self, educational_stage: &EducationalStage) -> bool {
+        match self {
+            EducationalStage::HighSchool {..} => {
+                if let EducationalStage::HighSchool {..} = educational_stage {
+                    true
+                } else {
+                    false
+                }
+            },
+            EducationalStage::College {..} => {
+                if let EducationalStage::College {..} = educational_stage {
+                    true
+                } else {
+                    false
+                }
+            },
+        }
+    }
+}
+
 pub struct Student <'a> {
     name: String,
     grade: Grade,
@@ -37,7 +58,7 @@ impl <'a> Class <'a> {
         let educational_stage = self.students.get(0).map(|s| s.educational_stage);
         
         match educational_stage {
-            Some(es) if es != student.educational_stage => {
+            Some(es) if !es.same_variant(student.educational_stage) => {
                 Err("Different Educational Stage")                 
             },
             _ => {
@@ -45,6 +66,18 @@ impl <'a> Class <'a> {
                 Ok(())
             }
         }            
+    }
+
+    pub fn same_institution(self) -> bool {
+        if self.students.is_empty() {
+            return false;
+        }
+
+        let educational_stage = self.students[0].educational_stage;
+
+        self.students.iter()
+            .map(|student| student.educational_stage)
+            .all(|es| es == educational_stage)
     }
 }
 
@@ -56,8 +89,9 @@ pub mod tests {
     #[test]
     fn enroll_student_ok() {
         let highschool= EducationalStage::HighSchool { name: String::from("Escuela 1") };
+        let highschool_2= EducationalStage::HighSchool { name: String::from("Escuela 2") };
         let student_1 = Student::new(String::from("Student 1"), Grade::Lower, &highschool);
-        let student_2 = Student::new(String::from("Student 2"), Grade::Higher, &highschool);
+        let student_2 = Student::new(String::from("Student 2"), Grade::Higher, &highschool_2);
 
         let mut class = Class::new(String::from("Class 1"), String::from("Professor 1"));
         let result_1 = class.enrroll_student(student_1);
@@ -82,5 +116,32 @@ pub mod tests {
         assert!(result_1.is_ok());
         assert!(result_2.is_err());
         assert_eq!(1, class.students.len());
+    }
+
+    #[test]
+    fn same_institution_true() {
+        let highschool= EducationalStage::HighSchool { name: String::from("Escuela 1") };
+        let student_1 = Student::new(String::from("Student 1"), Grade::Lower, &highschool);
+        let student_2 = Student::new(String::from("Student 2"), Grade::Higher, &highschool);
+
+        let mut class = Class::new(String::from("Class 1"), String::from("Professor 1"));
+        let _ = class.enrroll_student(student_1);
+        let _ = class.enrroll_student(student_2);
+
+        assert!(class.same_institution());
+    }
+
+    #[test]
+    fn same_institution_false() {
+        let highschool= EducationalStage::HighSchool { name: String::from("Escuela 1") };
+        let highschool_2= EducationalStage::HighSchool { name: String::from("Escuela 2") };
+        let student_1 = Student::new(String::from("Student 1"), Grade::Lower, &highschool);
+        let student_2 = Student::new(String::from("Student 2"), Grade::Higher, &highschool_2);
+
+        let mut class = Class::new(String::from("Class 1"), String::from("Professor 1"));
+        let _ = class.enrroll_student(student_1);
+        let _ = class.enrroll_student(student_2);
+
+        assert!(!class.same_institution());
     }
 }
